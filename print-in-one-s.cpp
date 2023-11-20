4358b4c8cd5a5ea6e8bdb56619878b8d627b56aa
Mahdi Khademi <125951715+PTuFR@users.noreply.github.com>
2023-11-20 18:49:34 +0330

    Add files via upload

---
+++ print-in-one-s.cpp
@@ -0,0 +1,18 @@
+//Mahdi khademi
+//Predator.m.khadem
+#include<bits/stdc++.h>
+#include<sstream>
+#include<conio.h>
+#include<math.h>
+#include<string>
+#include<stdio.h>
+#includectime>
+#include<windows.h>
+using namespace std;
+int main() {
+ while(1)
+    {
+        printf("Hello My Friend\n");
+        Sleep(1000);
+    }
+}
